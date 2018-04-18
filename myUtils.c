bad8bcd4ea0e49be91130206ca94cffbd54bb734
Matt23 <kamild706@gmail.com>
2018-04-18 23:29:41 +0200

    changed tabs to spaces

--- myUtils.c
+++ myUtils.c
@@ -10,11 +10,11 @@ char* concat(const char* s1, const char* s2) {
 }
 
 int endsWithSlash(char* str) {
-	int len = strlen(str);
-	char* lastChar = &str[len - 1];
-	if (strcmp(lastChar, "/") == 0)
-		return 1;
-	return 0;
+    int len = strlen(str);
+    char* lastChar = &str[len - 1];
+    if (strcmp(lastChar, "/") == 0)
+        return 1;
+    return 0;
 }
 
 char* appendSlash(const char* s1) {
